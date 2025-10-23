9e516ebb3c19824707c24f7a122a485ea0b219fd
syihwamoza <mozakastella26@gmail.com>
2025-10-23 16:33:15 +0700

    Menambahkan MainActivity dengan Scaffold dan memanggil ActivitasPertama (perbaiki)

--- app/src/main/java/com/example/praktikum3/MainActivity.kt
+++ app/src/main/java/com/example/praktikum3/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.UIPertama.ActivitasPertama
 import com.example.praktikum3.ui.theme.Praktikum3Theme
 
 class MainActivity : ComponentActivity() {
