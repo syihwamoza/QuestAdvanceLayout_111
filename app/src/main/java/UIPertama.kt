8cc8b39919e55387991ef2fa736816f992114e53
syihwamoza <mozakastella26@gmail.com>
2025-10-23 16:17:51 +0700

    menambahkan text untuk menampilkan prodi

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -34,4 +34,11 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            stringResource(R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
 
