367d056976d938e3e15698fc127590842cbc6474
syihwamoza <mozakastella26@gmail.com>
2025-10-23 16:32:48 +0700

    Menambahkan MainActivity dengan Scaffold dan memanggil ActivitasPertama

--- app/src/main/java/com/example/praktikum3/MainActivity.kt
+++ app/src/main/java/com/example/praktikum3/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum3Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum3Theme {
-        Greeting("Android")
     }
 }
