63bec5cd535a7cb58158ee5816a65b9026e01051
syihwamoza <mozakastella26@gmail.com>
2025-10-24 18:40:53 +0700

    Menambahkan elemen Image pada card biru

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -93,6 +93,15 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             )
         ) {
             Row {
+                val gambar = painterResource(R.drawable.download)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(all = 5.dp)
+                )
+            }
     }
 }
 
