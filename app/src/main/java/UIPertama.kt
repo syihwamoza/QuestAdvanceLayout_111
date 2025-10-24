06dcdd7d5b702684c1d3914b7aaf9c5334d410d8
syihwamoza <mozakastella26@gmail.com>
2025-10-24 19:00:29 +0700

    Menambahkan Text 'Moza Kastella' dengan font Cursive, ukuran 30sp, warna putih, dan padding atas 10dp di card merah

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -143,6 +143,12 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
                 Spacer(modifier = Modifier.width(30.dp))
                 Column {
                     Text(
+                        text = "Moza Kastella",
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
 
 
                 }}
